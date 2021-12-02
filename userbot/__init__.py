6b16516b1358ea5edae47136617d4084d788e44d
SakirBey1 <sakirhack81@gmail.com>
2021-12-02 16:08:46 +0300

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -1,6 +1,6 @@
 # Copyright (C) 2021 The HerlockProjects Company LLC.
 #
-# Licensed under the OwenProjects Public License, Version 1.c (the "License");
+# Licensed under the HerlockProjects Public License, Version 1.c (the "License");
 # you may not use this file except in compliance with the License.
 #
 
