c6dfcaf6897dcbf8a28f7d14ca80741d6e1a6f93
SakirBey1 <sakirhack81@gmail.com>
2021-12-16 18:07:11 +0300

    Update main.py

--- userbot/main.py
+++ userbot/main.py
@@ -4,7 +4,7 @@
 # you may not use this file except in compliance with the License.
 #
 
-# Owen UserBot - ErdewBey - ByMisakiMey
+# Herlock UserBot - SakirBey1 - ByMisakiMey
 
 """ UserBot başlangıç noktası """
 import importlib
@@ -93,7 +93,7 @@ CV_MSG = [
 ]
 
 
-UNAPPROVED_MSG = ("`Hey olduğun yerde kal,!👨‍💻 Ben Botum. Endişelenme!\n\n`"
+UNAPPROVED_MSG = ("`Hey olduğun yerde kal,!👨‍💻 Ben [HerlockUserBot](t.me/herlockuserbot1) um. Endişelenme!\n\n`"
                   "`Sahibim sana mesaj atma izni vermedi o yüzden sahibim seni onaylayana kadar bu mesajı alacaksın.. `"
                   "`Lütfen sahibimin aktif olmasını bekleyin, o genellikle PM'leri onaylar.\n\n`"
                   "`Bildiğim kadarıyla o kafayı yemiş insanlara PM izni vermiyor.`")
