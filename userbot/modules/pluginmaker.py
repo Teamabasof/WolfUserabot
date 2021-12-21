46a7fc81b313773be5249858283198f6966ccd17
SakirBey1 <sakirhack81@gmail.com>
2021-12-21 23:00:21 +0300

    Update pluginmaker.py

--- userbot/modules/pluginmaker.py
+++ userbot/modules/pluginmaker.py
@@ -50,7 +50,7 @@ async def _(q):
 						import userbot.helpers.p as edit
 						edit.e_(dosya_name, name, slep, liste)
 						#file = await q.client.upload_file(f'./fg{dosya_name}.py')
-						await q.client.send_file(q.chat_id, f"./herlockuserbot{dosya_name}.py", force_document=True, caption="Bu Plugin @HerlockUserBot Tarafından Yapılmıştır..")
+						await q.client.send_file(q.chat_id, f"./herlockuserbot{dosya_name}.py", force_document=True, caption="Bu Plugin @HerlockUserBot1 Tarafından Yapılmıştır..")
 						await q.delete()
 						os.remove(f"./herlockuserbot{dosya_name}.py")
 						return
@@ -66,7 +66,7 @@ async def _(q):
 						import userbot.helpers.p as edit
 						edit.a_(dosya_name, name, liste, slep)
 						#file = await q.client.upload_file(f'./fg{dosya_name}.py')
-						await q.client.send_file(q.chat_id, f"./herlockuserbot{dosya_name}.py", force_document=True, caption="Bu Plugin @HerlockUserBot Tarafından Yapılmıştır..")
+						await q.client.send_file(q.chat_id, f"./herlockuserbot{dosya_name}.py", force_document=True, caption="Bu Plugin @HerlockUserBot1 Tarafından Yapılmıştır..")
 						await q.delete()
 						os.remove(f"./herlockuserbot{dosya_name}.py")
 						return
@@ -80,7 +80,7 @@ async def _(q):
 						slep = sleep_t if sleep_t else 1.6
 						import userbot.helpers.p as edit
 						edit.r_(dosya_name, name, liste)
-						await q.client.send_file(q.chat_id, f"./herlockuserbot{dosya_name}.py", force_document=True, caption="Bu Plugin @HerlockUserBot Tarafından Yapılmıştır..")
+						await q.client.send_file(q.chat_id, f"./herlockuserbot{dosya_name}.py", force_document=True, caption="Bu Plugin @HerlockUserBot1 Tarafından Yapılmıştır..")
 						await q.delete()
 						os.remove(f"./herlockuserbot{dosya_name}.py")
 						return
@@ -93,7 +93,7 @@ async def _(q):
 						dosya_name=dosya_name+1
 						import userbot.helpers.p as edit
 						edit.m_(dosya_name, name, liste)
-						await q.client.send_file(q.chat_id, f"./herlockuserbot{dosya_name}.py", force_document=True, caption="Bu Plugin @HerlockUserBot Tarafından Yapılmıştır..")
+						await q.client.send_file(q.chat_id, f"./herlockuserbot{dosya_name}.py", force_document=True, caption="Bu Plugin @HerlockUserBot1 Tarafından Yapılmıştır..")
 						await q.delete()
 						os.remove(f"./herlockuserbot{dosya_name}.py")
 						return
@@ -112,6 +112,6 @@ async def _(q):
 		return
 
 c_ = c("pluginmaker")
-c_.add_command("pmake", "<pluginin_komutu> <plugin_hızı> <edit/alt/foto/muzik> ", "@HerlockUserBot Sizin İçin Bir plugin Oluşturur...")
+c_.add_command("pmake", "<pluginin_komutu> <plugin_hızı> <edit/alt/foto/muzik> ", "@HerlockUserBot Sizin1 İçin Bir plugin Oluşturur...")
 c_.add_info(' Thx to @SakirBey1').add()
 c_.add()
