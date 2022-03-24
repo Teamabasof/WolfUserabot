ef0719f673956e47d886f36dad6a7d0d925c458d
SakirBey1 <sakirhack81@gmail.com>
2022-03-25 00:29:25 +0300

    Update cevir.py

--- userbot/modules/cevir.py
+++ userbot/modules/cevir.py
@@ -45,7 +45,7 @@ async def cevir(event):
 
         im = Image.open(foto).convert("RGB")
         im.save("sticker.png", "png")
-        await event.client.send_file(event.chat_id, "sticker.png", reply_to=rep_msg, caption="@HerlockUserBot `ile Fotoğrafa çevirildi`")
+        await event.client.send_file(event.chat_id, "sticker.png", reply_to=rep_msg, caption="@HerlockUserBot1 `ile Fotoğrafa çevirildi`")
 
         await event.delete()
         os.remove("sticker.png")
@@ -70,7 +70,7 @@ async def cevir(event):
             indir = await rep_msg.download_media()
             ses = await asyncio.create_subprocess_shell(f"ffmpeg -i '{indir}' {KOMUT[efekt]} output.mp3")
             await ses.communicate()
-            await event.client.send_file(event.chat_id, "output.mp3", reply_to=rep_msg, caption="@HerlockUserBot `ile efekt uygulandı.`")
+            await event.client.send_file(event.chat_id, "output.mp3", reply_to=rep_msg, caption="@HerlockUserBot1 `ile efekt uygulandı.`")
             
             await event.delete()
             os.remove(indir)
@@ -120,7 +120,7 @@ async def cevir(event):
         await event.edit('`Ses yükleniyor...`')
         
         try:
-            await event.client.send_file(event.chat_id, "out.mp3",reply_to=rep_msg, caption='@HerlockUserBot ile sese çevrildi.')
+            await event.client.send_file(event.chat_id, "out.mp3",reply_to=rep_msg, caption='@HerlockUserBot1 ile sese çevrildi.')
         except:
             os.remove(video)
             return await event.edit('`Sese çevirilemedi!`')
