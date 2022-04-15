5e9c367b059bd270f9220d69c7c1ee81b7722b1f
SakirBey1 <sakirhack81@gmail.com>
2022-04-15 23:27:50 +0300

    Update siri.py

--- userbot/modules/siri.py
+++ userbot/modules/siri.py
@@ -7,9 +7,9 @@ from userbot.events import register
 @register(outgoing=True,pattern="^.[Ss]iri")
 
 async def komut(event):
-        await event.edit("**Alışkanlık İşte**\n __Botumuzun İsmi Değişti Unuttun mu Siri Yazmak Yerine__ `.herlock` __Yazmalısın.__\n \n**Gerekli Açıklama:** t.me/HerlockUserBot/65 \n UserBot Kanalı: @HerlockUserBot\nPlugin Kanalı: @HerlockPlugin")
+        await event.edit("**Alışkanlık İşte**\n __Botumuzun İsmi Değişti Unuttun mu Siri Yazmak Yerine__ `.herlock` __Yazmalısın.__\n \n**Gerekli Açıklama:** t.me/HerlockUserBot1/65 \n UserBot Kanalı: @HerlockUserBot1\nPlugin Kanalı: @HerlockPlugin1")
 
 @register(outgoing=True,pattern="^.[Oo]wen")
 
 async def komut(event):
-        await event.edit("**Alışkanlık İşte**\n __Botumuzun İsmi Değişti Unuttun mu Epic Yazmak Yerine__ `.herlock` __Yazmalısın.__\n \n**Gerekli Açıklama:** https://t.me/HerlockUserBot/80 \n UserBot Kanalı: @HerlockUserBot\nPlugin Kanalı: @HerlockPlugin")
+        await event.edit("**Alışkanlık İşte**\n __Botumuzun İsmi Değişti Unuttun mu Epic Yazmak Yerine__ `.herlock` __Yazmalısın.__\n \n**Gerekli Açıklama:** https://t.me/HerlockUserBot1/80 \n UserBot Kanalı: @HerlockUserBot1\nPlugin Kanalı: @HerlockPlugin1")
