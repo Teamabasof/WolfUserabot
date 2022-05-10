391445f7e4751c40279ff466a627e47c4973787f
Teamabasof <104113926+Teamabasof@users.noreply.github.com>
2022-05-10 12:25:45 +0400

    update __wolf.py

--- userbot/modules/__wolf.py
+++ userbot/modules/__wolf.py
@@ -35,7 +35,7 @@ async def wolf(event):
         sayfa = [sorted(list(CMD_HELP))[i:i + 5] for i in range(0, len(sorted(list(CMD_HELP))), 5)]
         
         for i in sayfa:
-            string += f'`☄️⇝ `'
+            string += f'`️🐺⇝ `'
             for sira, a in enumerate(i):
                 string += "`" + str(a)
                 if sira == i.index(i[-1]):
