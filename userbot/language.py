b6941ac3fea6580587938189feaa683d5e3d6638
Teamabasof <104113926+Teamabasof@users.noreply.github.com>
2022-05-10 13:20:50 +0400

    update language.py

--- userbot/language.py
+++ userbot/language.py
@@ -16,7 +16,7 @@ LOGS.info("Dil dosyası yükleniyor...")
 LANGUAGE_JSON = None
 
 for dil in bot.iter_messages(pchannel, filter=InputMessagesFilterDocument):
-    if ((len(dil.file.name.split(".")) >= 2) and (dil.file.name.split(".")[1] == "wolfjson")):
+    if ((len(dil.file.name.split(".")) >= 2) and (dil.file.name.split(".")[1] == "herlockjson")):
         if path.isfile(f"./userbot/language/{dil.file.name}"):
             try:
                 LANGUAGE_JSON = loads(open(f"./userbot/language/{dil.file.name}", "r").read())
@@ -24,9 +24,9 @@ for dil in bot.iter_messages(pchannel, filter=InputMessagesFilterDocument):
                 dil.delete()
                 remove(f"./userbot/language/{dil.file.name}")
 
-                if path.isfile("./userbot/language/DEFAULT.wolfjson"):
+                if path.isfile("./userbot/language/DEFAULT.herlockjson"):
                     LOGS.warn("Varsayılan dil dosyası kullanılıyor...")
-                    LANGUAGE_JSON = loads(open(f"./userbot/language/DEFAULT.wolfjson", "r").read())
+                    LANGUAGE_JSON = loads(open(f"./userbot/language/DEFAULT.herlockjson", "r").read())
                 else:
                     raise Exception("Your language file is invalid")
         else:
@@ -35,23 +35,23 @@ for dil in bot.iter_messages(pchannel, filter=InputMessagesFilterDocument):
                 LANGUAGE_JSON = loads(open(DOSYA, "r").read())
             except JSONDecodeError:
                 dil.delete()
-                if path.isfile("./userbot/language/DEFAULT.wolfjson"):
+                if path.isfile("./userbot/language/DEFAULT.herlockjson"):
                     LOGS.warn("Varsayılan dil dosyası kullanılıyor...")
-                    LANGUAGE_JSON = loads(open(f"./userbot/language/DEFAULT.wolfjson", "r").read())
+                    LANGUAGE_JSON = loads(open(f"./userbot/language/DEFAULT.herlockjson", "r").read())
                 else:
                     raise Exception("Your language file is invalid")
         break
 
 if LANGUAGE_JSON == None:
-    if path.isfile(f"./userbot/language/{LANGUAGE}.wolfjson"):
+    if path.isfile(f"./userbot/language/{LANGUAGE}.herlockjson"):
         try:
-            LANGUAGE_JSON = loads(open(f"./userbot/language/{LANGUAGE}.wolfjson", "r").read())
+            LANGUAGE_JSON = loads(open(f"./userbot/language/{LANGUAGE}.herlockjson", "r").read())
         except JSONDecodeError:
             raise Exception("Invalid json file")
     else:
-        if path.isfile("./userbot/language/DEFAULT.wolfjson"):
+        if path.isfile("./userbot/language/DEFAULT.herlcokjson"):
             LOGS.warn("Varsayılan dil dosyası kullanılıyor...")
-            LANGUAGE_JSON = loads(open(f"./userbot/language/DEFAULT.wolfjson", "r").read())
+            LANGUAGE_JSON = loads(open(f"./userbot/language/DEFAULT.herlockjson", "r").read())
         else:
             raise Exception(f"Didn't find {LANGUAGE} file")
 
